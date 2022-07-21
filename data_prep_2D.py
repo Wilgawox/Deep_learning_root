0ac82b7bc28193cae7e608d95ff1ff3a38bd9392
Rocsg <romainfernandez06@gmail.com>
2022-07-21 15:07:20 +0200

    First feature up

--- data_prep_2D.py
+++ data_prep_2D.py
@@ -1,11 +1,11 @@
 
 import numpy as np
 import ranging_and_tiling_helpers
-import paths
+import os
+#import paths
 
 
-
-def data_arborescence_setup_splitter(list_X, list_Y) :
+def data_arborescence_setup_splitter(list_X, list_Y,paths) :
     #Function not working anymore, keeping it here for now if I need it later on
     # Process and save the images, putting them in different folders to serve differents puposes
    for i in range(0, len(list_X)) :
@@ -31,14 +31,15 @@ def data_arborescence_setup_splitter(list_X, list_Y) :
 
 
 
-def data_arborescence_setup(list_X, list_Y) : 
+def data_arborescence_setup(list_X, list_Y,paths) : 
     # Save X and Y as .npy in the dataset path, tiled and ranged
+    os.mkdir(paths['dataset_path'])
     for i in range(1, len(list_X)+1) :
         print(i)
         for j in range(1, len(list_X[i-1])+1):
             # Ranging and tiling the images
-            tilesX = ranging_and_tiling_helpers.img_tile_and_range(list_X[i-1][j-1], paths.INT_ROOT, paths.INT_BG, paths.TILE_SIZE, paths.STRIDE)
-            tilesY = ranging_and_tiling_helpers.tiling(list_Y[i-1][j-1], paths.TILE_SIZE, paths.STRIDE)
+            tilesX = ranging_and_tiling_helpers.img_tile_and_range(list_X[i-1][j-1], paths['int_root'], paths['int_bg'], paths['tile_size'], paths['stride'])
+            tilesY = ranging_and_tiling_helpers.tiling(list_Y[i-1][j-1], paths['tile_size'], paths['stride'])
             if len(tilesX)!=len(tilesY) :
                 # Check for different size tiles, which would be a big problem later on
                 raise Exception('Problem while cutting tiles', 'Different number of tiles')
@@ -50,6 +51,6 @@ def data_arborescence_setup(list_X, list_Y) :
                         if(i<1000):strI="0"+str(i)
                         else:
                             strI=""+str(i)
-                print((paths.dataset_path+'ML1_input_img0'+strI+'.time'+str(j)+'.number'+str(k)))
-                np.save((paths.dataset_path+'ML1_input_img0'+strI+'.time'+str(j)+'.number'+str(k)), tilesX[k-1])
-                np.save((paths.dataset_path+'ML1_result_img0'+strI+'.time'+str(j)+'.number'+str(k)), tilesY[k-1])
+                print((paths['dataset_path']+'ML1_input_img0'+strI+'.time'+str(j)+'.number'+str(k)))
+                np.save((paths['dataset_path']+'ML1_input_img0'+strI+'.time'+str(j)+'.number'+str(k)), tilesX[k-1])
+                np.save((paths['dataset_path']+'ML1_result_img0'+strI+'.time'+str(j)+'.number'+str(k)), tilesY[k-1])
